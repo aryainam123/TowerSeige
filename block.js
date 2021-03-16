f33103c8ab30924f39a41a65a776bdb31abb310c
Rashmi Deshmukh <57807833+rashmi-deshmukh@users.noreply.github.com>
2021-03-16 15:55:09 +0530

    Update block.js

--- block.js
+++ block.js
@@ -1,11 +1,7 @@
 class Block{
     constructor(x, y, width, height) {
         var options = {
-           
-            restitution :0.4,
-            friction :0.0,
-            //isStatic:true
-           
+                     
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
